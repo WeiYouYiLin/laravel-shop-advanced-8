246aabd06be9147b140d88ee957efca908cf80b4
WeiYouYiLin <1450580734@qq.com>
2019-03-26 07:58:08 +0000

    邮箱验证功能

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@
 
 
 Route::get('/', 'PagesController@root')->name('root');
-Auth::routes();
+// 在之前的路由里加上一个 verify 参数
+Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
