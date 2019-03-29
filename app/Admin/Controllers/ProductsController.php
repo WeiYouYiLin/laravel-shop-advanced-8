62485561ee84b2b37669638d0b7d88cb806791a0
WeiYouYiLin <1450580734@qq.com>
2019-03-29 07:06:19 +0000

    管理后台 - 商品列表

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -22,9 +22,12 @@ class ProductsController extends Controller
      */
     public function index(Content $content)
     {
-        return $content
+        /*return $content
             ->header('Index')
             ->description('description')
+            ->body($this->grid());*/
+        return $content
+            ->header('商品列表')
             ->body($this->grid());
     }
 
@@ -79,7 +82,7 @@ class ProductsController extends Controller
      */
     protected function grid()
     {
-        $grid = new Grid(new Product);
+        /*$grid = new Grid(new Product);
 
         $grid->id('Id');
         $grid->title('Title');
@@ -92,6 +95,31 @@ class ProductsController extends Controller
         $grid->price('Price');
         $grid->created_at('Created at');
         $grid->updated_at('Updated at');
+
+        return $grid;*/
+
+        $grid = new Grid(new Product);
+
+        $grid->id('ID')->sortable();
+        $grid->title('商品名称');
+        $grid->on_sale('已上架')->display(function ($value) {
+            return $value ? '是' : '否';
+        });
+        $grid->price('价格');
+        $grid->rating('评分');
+        $grid->sold_count('销量');
+        $grid->review_count('评论数');
+
+        $grid->actions(function ($actions) {
+            $actions->disableView();
+            $actions->disableDelete();
+        });
+        $grid->tools(function ($tools) {
+            // 禁用批量删除按钮
+            $tools->batch(function ($batch) {
+                $batch->disableDelete();
+            });
+        });
 
         return $grid;
     }
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -11,6 +11,9 @@ Route::group([
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index');
+    // 后台用户管理
     $router->get('users', 'UsersController@index');
+    // 后台商品管理
+    $router->get('products', 'ProductsController@index');
 
 });
