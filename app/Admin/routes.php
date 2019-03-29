1c3c64276f0f1b458c8d89e3e8923ec24f7a3246
WeiYouYiLin <1450580734@qq.com>
2019-03-29 09:50:32 +0000

    管理后台 - 新增、编辑商品

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,7 +13,14 @@ Route::group([
     $router->get('/', 'HomeController@index');
     // 后台用户管理
     $router->get('users', 'UsersController@index');
-    // 后台商品管理
+    // 后台商品管理列表
     $router->get('products', 'ProductsController@index');
-
+    // 创建商品页面
+    $router->get('products/create', 'ProductsController@create');
+    // 创建商品操作
+    $router->post('products', 'ProductsController@store');
+    // 编辑商品页面
+    $router->get('products/{id}/edit', 'ProductsController@edit');
+    // 编辑商品操作
+	$router->put('products/{id}', 'ProductsController@update');
 });
