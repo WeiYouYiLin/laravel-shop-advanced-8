0ea357e7428b780c67c7b6f497183ca4fe118b33
WeiYouYiLin <1450580734@qq.com>
2019-04-03 09:36:32 +0000

    创建订单

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('cart', 'CartController@index')->name('cart.index');
     // 移除购物车商品
     Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+    // 创建订单
+    Route::post('orders', 'OrdersController@store')->name('orders.store');
 });
 
 // 产品详情 （为避免与用户收藏列表冲突，将路由放在收藏列表路由的下方）
