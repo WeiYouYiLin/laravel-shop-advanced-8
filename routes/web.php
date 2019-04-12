b4b507c4df8d1f57fa0737b2b5b0745a67fe0f90
WeiYouYiLin <1450580734@qq.com>
2019-04-12 09:40:22 +0000

    用户界面 - 检查优惠劵

--- routes/web.php
+++ routes/web.php
@@ -73,6 +73,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     // 申请退款的接口
     Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
 
+    // 检查优惠劵
+    Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 });
 
 // 产品详情 （为避免与用户收藏列表冲突，将路由放在收藏列表路由的下方）
