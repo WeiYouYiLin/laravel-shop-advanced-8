cd1f3af8611cc49dcaf4c184e2153ae59d5a27c1
WeiYouYiLin <1450580734@qq.com>
2021-03-30 03:44:23 +0000

    管理后台 - 用户列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,13 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Encore\Admin\Traits\DefaultDatetimeFormat;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
+    // 加上这个 Trait
+    use DefaultDatetimeFormat;
 
     /**
      * The attributes that are mass assignable.
